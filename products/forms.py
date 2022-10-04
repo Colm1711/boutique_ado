ae53870d7505c8644385ce93efbe767b5407db51
Colm1711 <cosullivan1711@gmail.com>
2022-10-04 17:33:21 +0000

    Product Admin - add product view, url and tmeplate

--- products/forms.py
+++ products/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Product, Category
 
 
-class ProductForm(models.ModelForm):
+class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
@@ -11,7 +11,7 @@ class ProductForm(models.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_firendlyname()) for c in categories]
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
